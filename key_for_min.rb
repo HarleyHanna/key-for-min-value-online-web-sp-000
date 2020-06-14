d8b9ae9ea9a4265783bb4cf58248c46ec5d06a22
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-14 13:23:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
  final_key = ""
  name_hash.each do |key, value|
    if name_has.length == 0
+      final_key = nil
     elsif value < lowest
      final_key = key
      lowest = value
